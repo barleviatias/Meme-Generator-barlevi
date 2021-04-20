2f2263e60ed037a08b6f95e777215a7b4b3a4128
barleviatias <barlevi.atias@gmail.com>
2021-04-20 18:22:59 +0300

    basic function

--- js/controller.js
+++ js/controller.js
@@ -35,30 +35,33 @@ function renderMeme() {
 }
 function drawImg() {
     // var memes=getMemes();
-    gImg.src = 'img/1.jpg';
+    const meme = getMemes();
+    gImg.src = `img/${meme.selectedImgId}.jpg`;
     gCtx.drawImage(gImg, 0, 0, gCanvas.width, gCanvas.height)
         // drawText(memes.lines[0].txt, 255, 100)
     
-}
-function clearCanvas() {
-    gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height)
 }
 function txtEdit(txt) {
     var meme=getMemes();
     console.log(meme);
     drawText()
 }
-
+function onChangeFontSize(val) {
+    changeFontSize(val);
+    console.log('font change');
+    renderMeme();
+}
 function drawText() {
     const meme = getMemes();
+    const x = gCanvas.width / 2;
     meme.lines.forEach(line => {
         gCtx.textAlign = `${line.align}`;
         gCtx.font = `${line.size}px ${line.font}`;
         gCtx.fillStyle = `${line.color}`;
-        gCtx.fillText(`${line.txt}`, line.pos.x, line.pos.y);
+        gCtx.fillText(`${line.txt}`, x, line.pos.y);
         gCtx.lineWidth = 3;
         gCtx.strokeStyle = line.stroke;
-        gCtx.strokeText(`${line.txt}`, line.pos.x, line.pos.y);
+        gCtx.strokeText(`${line.txt}`, x, line.pos.y);
     })
     // document.querySelector('.text-editor input').value = meme.lines[meme.selectedLineIdx].txt;
 }
@@ -67,4 +70,24 @@ function drawText() {
 function toggleGenerator() {
     document.querySelector('.meme-editor').classList.toggle('no-display')
     document.querySelector('.gallery').classList.toggle('no-display')
+}
+
+function onChangeLineTxt(txt) {
+    changeLineTxt(txt)
+	renderMeme();
+}
+function onMoveLine(val) {
+    moveLine(val);
+    renderMeme();
+}
+function onChangeLine() {
+    const meme = getMemes();
+    changeLine()
+    renderMeme();
+    document.querySelector('.control-panel input').value = meme.lines[meme.selectedLineIdx].txt;
+    document.querySelector('.control-panel input').focus();
+
+}
+function clearCanvas() {
+    gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height)
 }
--- js/service-genrator.js
+++ js/service-genrator.js
@@ -3,19 +3,19 @@
 var gKeywords = { happy: 12, 'funny puk': 1 };
 var gImgs = [{ id: 1, url: 'img/popo.jpg', keywords: ['happy'] }];
 var gMeme = {
-	selectedImgId: 5,
+	selectedImgId: 1,
 	selectedLineIdx: 0,
 	lines: [
         {
             txt: 'Top Text',
-            size: 40,
+            size: 100,
             align: 'center',
             color: 'white',
             stroke: 'black',
             font: 'Impact',
             pos:
             {
-                x: 400,
+                x: 255,
                 y: 100
             }
         },
@@ -29,30 +29,48 @@ var gMeme = {
             font: 'Impact',
             pos:
             {
-                x: 400,
-                y: 750
+                x: 255,
+                y: 550
             }
         }
     ]
 };
 
 function changeMemeImg(id) {
-    gMeme.imgId = id;
+    gMeme.selectedImgId = id;
 }
 function changeLineTxt(txt) {
-	gMeme.lines[0].txt=txt;
+	gMeme.lines[gMeme.selectedLineIdx].txt=txt;
 }
 
 function getMemes() {
 	return gMeme
 }
-
-function incFont() {
-	gMeme.lines[0].size+=2;
+function changeFontSize(val) {
+    gMeme.lines[gMeme.selectedLineIdx].size += val;
+}
+function moveLine(val) {
+    gMeme.lines[gMeme.selectedLineIdx].pos.y += val;
+}
+function changeLine() {
+	gMeme.selectedLineIdx++;
+	if (gMeme.selectedLineIdx===gMeme.lines.length)gMeme.selectedLineIdx=0;
 }
 
-function dicresFont() {
-	gMeme.lines[0].size-=2;
+function createLine() {
+    const line = {
+        txt: 'New Text',
+        size: 100,
+        align: 'center',
+        color: 'white',
+        stroke: 'black',
+        font: 'Impact',
+        pos:
+        {
+            x: 400,
+            y: 400
+        }
+    }
+    return line;
 }
-
 
